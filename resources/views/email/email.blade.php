5b2975c6e03baa70917c5819510b7ebccf4ef215
Phung The Anh <phungtheanh2001@gmail.com>
2023-02-05 11:28:29 +0700

    3

---
+++ resources/views/email/email.blade.php
@@ -0,0 +1,23 @@
+<style>
+    * {
+        font-family: Arial, sans-serif;
+        color: #212121 !important;
+    }
+</style>
+
+<div style="padding: 10px;background: #fff;">
+    <div style="border-bottom: 1px dashed #e2e2e2;padding-bottom: 1px;margin-bottom: 10px;">
+        <img src="https://inet.vn/public/img/bank/techcombank-logo.png" alt="">
+        <h3 style="color: #212121">Ngân hàng TMCP Kỹ Thương Việt Nam - Techcombank</h3>
+        <p style="color: #212121; font-size: 16px">Phòng giao dịch Trần Đăng Ninh - chi nhánh hoàng Quốc Việt - TP Hà
+            Nội</p>
+    </div>
+    <div class="infor">
+        <p style="color: #212121; font-size: 14px">Số tài khoản: <strong>4938051001</strong></p>
+        <p style="color: #212121; font-size: 14px">Chủ tài khoản:<strong>CONG TY TNHH V-DONE</strong></p>
+        <p style="color: #212121; font-size: 14px">Nội dung chuyển khoản:<strong>Thanh toan don hang 1057792</strong>
+        </p>
+    </div>
+</div>
+
+
