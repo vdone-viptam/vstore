87bcba13f11292e9b63acf90e59457275d33b7ee
unknown <ttkhoa1999@gmail.com>
2023-02-09 13:07:31 +0700

    UP

--- resources/views/screens/manufacture/index.blade.php
+++ resources/views/screens/manufacture/index.blade.php
@@ -333,8 +333,8 @@
     $('.slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
+        autoplay: true,
+        autoplaySpeed: 2000,
         arrows: false,
         dots: true,
         });
