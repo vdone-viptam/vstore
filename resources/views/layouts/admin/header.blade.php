0965fac394a0b123d4236bbcf05dcd60cb505bdf
unknown <ttkhoa1999@gmail.com>
2023-02-21 15:45:39 +0700

    UP

--- resources/views/layouts/admin/header.blade.php
+++ resources/views/layouts/admin/header.blade.php
@@ -1,39 +1,9 @@
-<header class="hidden md:flex items-center justify-end gap-11 pt-8 pb-4 px-12 header">
-    <div class="help relative cursor-pointer lg:flex items-center gap-3 justify-end">
-        <div class="xl:w-[35px] lg:w-[30px] md:w-[26px]">
-            <svg viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect width="35" height="35" rx="12" fill="#F5F5F5"/>
-                <path
-                    d="M17.5 8.5C12.5312 8.5 8.5 12.5312 8.5 17.5C8.5 22.4687 12.5312 26.5 17.5 26.5C22.4687 26.5 26.5 22.4687 26.5 17.5C26.5 12.5312 22.4687 8.5 17.5 8.5ZM17.2187 22.75C17.0333 22.75 16.8521 22.695 16.6979 22.592C16.5437 22.489 16.4236 22.3426 16.3526 22.1713C16.2817 22 16.2631 21.8115 16.2993 21.6296C16.3354 21.4477 16.4247 21.2807 16.5558 21.1496C16.6869 21.0185 16.854 20.9292 17.0359 20.893C17.2177 20.8568 17.4062 20.8754 17.5775 20.9464C17.7488 21.0173 17.8952 21.1375 17.9983 21.2916C18.1013 21.4458 18.1562 21.6271 18.1562 21.8125C18.1562 22.0611 18.0575 22.2996 17.8817 22.4754C17.7058 22.6512 17.4674 22.75 17.2187 22.75ZM18.7862 17.9687C18.0264 18.4787 17.9219 18.9461 17.9219 19.375C17.9219 19.549 17.8527 19.716 17.7297 19.839C17.6066 19.9621 17.4397 20.0312 17.2656 20.0312C17.0916 20.0312 16.9247 19.9621 16.8016 19.839C16.6785 19.716 16.6094 19.549 16.6094 19.375C16.6094 18.348 17.0819 17.5314 18.0541 16.8784C18.9578 16.2719 19.4687 15.8875 19.4687 15.0423C19.4687 14.4677 19.1406 14.0312 18.4614 13.7083C18.3016 13.6323 17.9458 13.5583 17.508 13.5634C16.9586 13.5705 16.532 13.7017 16.2034 13.9661C15.5837 14.4648 15.5312 15.0077 15.5312 15.0156C15.5271 15.1018 15.506 15.1863 15.4692 15.2644C15.4324 15.3424 15.3805 15.4124 15.3167 15.4704C15.2528 15.5284 15.1781 15.5732 15.0969 15.6024C15.0157 15.6315 14.9295 15.6444 14.8434 15.6402C14.7572 15.6361 14.6727 15.615 14.5946 15.5782C14.5166 15.5414 14.4466 15.4895 14.3886 15.4256C14.3306 15.3618 14.2857 15.2871 14.2566 15.2059C14.2275 15.1247 14.2146 15.0385 14.2187 14.9523C14.2239 14.8384 14.3031 13.8123 15.3798 12.9461C15.9381 12.497 16.6483 12.2636 17.4892 12.2533C18.0845 12.2462 18.6437 12.347 19.023 12.5261C20.1578 13.0628 20.7812 13.9577 20.7812 15.0423C20.7812 16.6281 19.7214 17.3402 18.7862 17.9687Z"
-                    fill="#A2A1A1"/>
-            </svg>
-        </div>
-        <p class="text-black font-normal text-sm">Hỗ trợ</p>
-        <ul class="sub-nav-help">
-            <li><a href="#">Hướng dẫn sử dụng</a></li>
-            <li><a href="#">Biểu phí</a></li>
-            <li><a href="#">Chính sách quy định</a></li>
-        </ul>
-    </div>
-    <div class="flex justify-between md:gap-2 lg:gap-0 lg:justify-end items-center">
-        <div class="help relative lg:hidden flex items-center gap-3 justify-end">
-            <div class="xl:w-[35px] lg:w-[30px] md:w-[26px]">
-                <svg viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="35" height="35" rx="12" fill="#F5F5F5"/>
-                    <path
-                        d="M17.5 8.5C12.5312 8.5 8.5 12.5312 8.5 17.5C8.5 22.4687 12.5312 26.5 17.5 26.5C22.4687 26.5 26.5 22.4687 26.5 17.5C26.5 12.5312 22.4687 8.5 17.5 8.5ZM17.2187 22.75C17.0333 22.75 16.8521 22.695 16.6979 22.592C16.5437 22.489 16.4236 22.3426 16.3526 22.1713C16.2817 22 16.2631 21.8115 16.2993 21.6296C16.3354 21.4477 16.4247 21.2807 16.5558 21.1496C16.6869 21.0185 16.854 20.9292 17.0359 20.893C17.2177 20.8568 17.4062 20.8754 17.5775 20.9464C17.7488 21.0173 17.8952 21.1375 17.9983 21.2916C18.1013 21.4458 18.1562 21.6271 18.1562 21.8125C18.1562 22.0611 18.0575 22.2996 17.8817 22.4754C17.7058 22.6512 17.4674 22.75 17.2187 22.75ZM18.7862 17.9687C18.0264 18.4787 17.9219 18.9461 17.9219 19.375C17.9219 19.549 17.8527 19.716 17.7297 19.839C17.6066 19.9621 17.4397 20.0312 17.2656 20.0312C17.0916 20.0312 16.9247 19.9621 16.8016 19.839C16.6785 19.716 16.6094 19.549 16.6094 19.375C16.6094 18.348 17.0819 17.5314 18.0541 16.8784C18.9578 16.2719 19.4687 15.8875 19.4687 15.0423C19.4687 14.4677 19.1406 14.0312 18.4614 13.7083C18.3016 13.6323 17.9458 13.5583 17.508 13.5634C16.9586 13.5705 16.532 13.7017 16.2034 13.9661C15.5837 14.4648 15.5312 15.0077 15.5312 15.0156C15.5271 15.1018 15.506 15.1863 15.4692 15.2644C15.4324 15.3424 15.3805 15.4124 15.3167 15.4704C15.2528 15.5284 15.1781 15.5732 15.0969 15.6024C15.0157 15.6315 14.9295 15.6444 14.8434 15.6402C14.7572 15.6361 14.6727 15.615 14.5946 15.5782C14.5166 15.5414 14.4466 15.4895 14.3886 15.4256C14.3306 15.3618 14.2857 15.2871 14.2566 15.2059C14.2275 15.1247 14.2146 15.0385 14.2187 14.9523C14.2239 14.8384 14.3031 13.8123 15.3798 12.9461C15.9381 12.497 16.6483 12.2636 17.4892 12.2533C18.0845 12.2462 18.6437 12.347 19.023 12.5261C20.1578 13.0628 20.7812 13.9577 20.7812 15.0423C20.7812 16.6281 19.7214 17.3402 18.7862 17.9687Z"
-                        fill="#A2A1A1"/>
-                </svg>
-            </div>
-            <p class="text-black font-normal text-sm">Hỗ trợ</p>
-            <ul class="sub-nav-help">
-                <li><a href="#">Hướng dẫn sử dụng</a></li>
-                <li><a href="#">Biểu phí</a></li>
-                <li><a href="#">Chính sách quy định</a></li>
-            </ul>
-        </div>
-        <div class=" flex justify-end gap-6 xl:justify-end lg:justify-between items-center xl:gap-6">
-            <div class="notify relative cursor-pointer">
+<header class="top-0 hidden md:block " >
+<div class=" w-full bg-[#F2F8FF] nav py-2 xl:px-12 px-4 fixed top-0 right-0 z-[6]">
+<div class="flex justify-end lg:justify-end items-center">
+        <div
+            class="flex justify-end gap-6 xl:justify-end lg:justify-between items-center xl:gap-6 py-4 md:py-0">
+            <div class="notify relative cursor-pointer ">
                 <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
                      xmlns="http://www.w3.org/2000/svg">
                     <path
@@ -41,38 +11,43 @@
                         fill="#637381"/>
                 </svg>
                 <div class="w-[10px] h-[10px] bg-[#FF4842] rounded-[50%] absolute top-1.5 right-0"></div>
-                <div>
-                    <ul class="sub-nav-notify">
-                        <div class="flex justify-between items-center w-full pb-3 px-3">
-                            <h2 class="text-xl font-normal text-title">Thông báo</h2>
-                            <a href="{{route('admin_all_noti')}}" class="hover:text-primary duration-200 transition-all text-title font-medium">Tất
-                                cả</a>
-                        </div>
-                        @if(count(Auth::user()->unreadNotifications) > 0)
-                            @foreach (Auth::user()->unreadNotifications as $index =>$notification)
-                                <li>
-                                    <a href="{{$notification['data']['href']}}?noti_id={{$notification->id}}"
-                                       class="flex justify-between items-center w-full text-sm text-title font-bold"><span>{{$notification['data']['message']}} </span>
-                                        <span>{{\Illuminate\Support\Carbon::parse($notification->created_at)->format('h:i A')}} </span></a>
-                                </li>
-                            @endforeach
-                        @else
-                            <div class="text-center"><p>Bạn chưa có thông báo mới nào</p></div>
-                        @endif
+                <ul class="sub-nav-notify">
+                    <div class="flex justify-between items-center w-full pb-3 px-3">
+                        <h2 class="text-xl font-normal text-title">Thông báo</h2>
+                        <a href="{{route('ncc_all_noti')}}"
+                           class="hover:text-primary duration-200 transition-all text-title font-medium">Tất
+                            cả</a>
+                    </div>
+                    @if(count(Auth::user()->unreadNotifications) > 0)
+                        @foreach (Auth::user()->unreadNotifications as $index =>$notification)
+                            <li>
+                                <a href="{{$notification['data']['href']}}&noti_id={{$notification->id}}"
+                                   class="flex justify-between items-center w-full text-sm text-title font-bold">{{$notification['data']['message']}}
+                                    <span>{{\Illuminate\Support\Carbon::parse($notification->created_at)->format('h:i A')}} </span></a>
+                            </li>
+                        @endforeach
+                    @else
+                        <div class="text-center"><p>Bạn chưa có thông báo mới nào</p></div>
+                    @endif
 
 
-                    </ul>
-                </div>
+                </ul>
             </div>
             <div class="user relative flex items-center gap-2">
-{{--                <img class="w-[32px] h-[32px] rounded-[50%] cursor-pointer"--}}
-{{--                     src="{{asset('image/users/'.\Illuminate\Support\Facades\Auth::user()->avatar) ?? asset('asset/images/success.png')}}">--}}
+                <div class="w-[51px]">
+                    <div class="w-[50px] h-[65px] ">
+                        <img class="w-full cursor-pointer"
+                             src="{{asset('asset/images/userVstore.png')}}" class="w-full">
+                    </div>
+                </div>
+
+
                 <div class="flex flex-col gap-[3px] justify-center">
-                    <p class="text-black 2xl:text-base xl:text-sm font-medium cursor-pointer">{{\Illuminate\Support\Facades\Auth::user()->name}}</p>
+                    <p class="text-black 2xl:text-base xl:text-sm font-medium cursor-pointer">ID:{{\Illuminate\Support\Facades\Auth::user()->account_code}}</p>
                 </div>
 
                 <ul class="sub-nav-user">
-                    <li><a href="{{route('screens.admin.account.profile')}}"
+                    <li><a href="{{route('screens.manufacture.account.profile')}}"
                            class="font-medium flex justify-start items-center gap-2">
                             <svg width="18" height="18" viewBox="0 0 24 24" fill="none"
                                  xmlns="http://www.w3.org/2000/svg">
@@ -85,34 +60,39 @@
                     <li><a href="{{route('logout')}}"
                            class="font-medium flex justify-start items-center gap-2 ">
                             <svg fill="#FF4D4F" height="18" width="18" version="1.1" id="Capa_1"
-                                 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                 xmlns="http://www.w3.org/2000/svg"
+                                 xmlns:xlink="http://www.w3.org/1999/xlink"
                                  viewBox="0 0 384.971 384.971" xml:space="preserve">
-                                   <g>
-                                       <g id="Sign_Out">
-                                           <path d="M180.455,360.91H24.061V24.061h156.394c6.641,0,12.03-5.39,12.03-12.03s-5.39-12.03-12.03-12.03H12.03
-                                               C5.39,0.001,0,5.39,0,12.031V372.94c0,6.641,5.39,12.03,12.03,12.03h168.424c6.641,0,12.03-5.39,12.03-12.03
-                                               C192.485,366.299,187.095,360.91,180.455,360.91z"/>
-                                           <path d="M381.481,184.088l-83.009-84.2c-4.704-4.752-12.319-4.74-17.011,0c-4.704,4.74-4.704,12.439,0,17.179l62.558,63.46H96.279
-                                               c-6.641,0-12.03,5.438-12.03,12.151c0,6.713,5.39,12.151,12.03,12.151h247.74l-62.558,63.46c-4.704,4.752-4.704,12.439,0,17.179
-                                               c4.704,4.752,12.319,4.752,17.011,0l82.997-84.2C386.113,196.588,386.161,188.756,381.481,184.088z"/>
-                                       </g>
-                                       <g>
-                                       </g>
-                                       <g>
-                                       </g>
-                                       <g>
-                                       </g>
-                                       <g>
-                                       </g>
-                                       <g>
-                                       </g>
-                                       <g>
-                                       </g>
-                                   </g>
-                                   </svg>
+                       <g>
+                           <g id="Sign_Out">
+                               <path d="M180.455,360.91H24.061V24.061h156.394c6.641,0,12.03-5.39,12.03-12.03s-5.39-12.03-12.03-12.03H12.03
+                                   C5.39,0.001,0,5.39,0,12.031V372.94c0,6.641,5.39,12.03,12.03,12.03h168.424c6.641,0,12.03-5.39,12.03-12.03
+                                   C192.485,366.299,187.095,360.91,180.455,360.91z"/>
+                               <path d="M381.481,184.088l-83.009-84.2c-4.704-4.752-12.319-4.74-17.011,0c-4.704,4.74-4.704,12.439,0,17.179l62.558,63.46H96.279
+                                   c-6.641,0-12.03,5.438-12.03,12.151c0,6.713,5.39,12.151,12.03,12.151h247.74l-62.558,63.46c-4.704,4.752-4.704,12.439,0,17.179
+                                   c4.704,4.752,12.319,4.752,17.011,0l82.997-84.2C386.113,196.588,386.161,188.756,381.481,184.088z"/>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                           <g>
+                           </g>
+                       </g>
+                       </svg>
                             Đăng xuất</a></li>
                 </ul>
             </div>
         </div>
     </div>
+</div>
+<div class="h-[80px]">
+
+</div>
 </header>
