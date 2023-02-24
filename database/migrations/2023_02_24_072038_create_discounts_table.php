79d6af0a9627cb6c252ca73597ac06bfee307c6b
phongngo2002 <phongcuoi2213@gmail.com>
2023-02-24 14:29:46 +0700

    up

---
+++ database/migrations/2023_02_24_072038_create_discounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('type')->default(0);
+            $table->integer('product_id');
+            $table->double('discount', 10, 2);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+};
