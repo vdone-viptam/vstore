ea538e206a7b4e4c6e0d7eaf10334339e8a18ff3
unknown <ttkhoa1999@gmail.com>
2023-02-27 11:40:54 +0700

    UP

--- resources/views/layouts/manufacture/header.blade.php
+++ resources/views/layouts/manufacture/header.blade.php
@@ -3,37 +3,11 @@
 <div class="flex justify-end lg:justify-end items-center">
         <div
             class="flex justify-end gap-6 xl:justify-end lg:justify-between items-center xl:gap-6 py-4 md:py-0">
-            <div class="notify relative cursor-pointer ">
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M13.2841 25.6372C13.9506 25.4962 18.0122 25.4962 18.6788 25.6372C19.2486 25.7688 19.8648 26.0763 19.8648 26.7477C19.8316 27.387 19.4566 27.9536 18.9385 28.3135C18.2666 28.8373 17.4782 29.169 16.6539 29.2885C16.1981 29.3476 15.7502 29.3489 15.3102 29.2885C14.4847 29.169 13.6962 28.8373 13.0257 28.3122C12.5062 27.9536 12.1312 27.387 12.0981 26.7477C12.0981 26.0763 12.7143 25.7688 13.2841 25.6372ZM16.0601 2.66663C18.8337 2.66663 21.6668 3.98265 23.3498 6.16618C24.4417 7.57218 24.9426 8.97683 24.9426 11.1604V11.7284C24.9426 13.403 25.3852 14.39 26.3592 15.5274C27.0973 16.3654 27.3332 17.441 27.3332 18.608C27.3332 19.7736 26.9502 20.8801 26.1829 21.7785C25.1785 22.8555 23.7619 23.5431 22.3162 23.6626C20.2211 23.8412 18.1247 23.9916 16.0005 23.9916C13.875 23.9916 11.7799 23.9016 9.68484 23.6626C8.23778 23.5431 6.8212 22.8555 5.81806 21.7785C5.0508 20.8801 4.6665 19.7736 4.6665 18.608C4.6665 17.441 4.90371 16.3654 5.64049 15.5274C6.64495 14.39 7.0584 13.403 7.0584 11.7284V11.1604C7.0584 8.91775 7.61761 7.45132 8.76916 6.01578C10.4813 3.92222 13.2256 2.66663 15.9409 2.66663H16.0601Z"
-                        fill="#637381"/>
-                </svg>
-                <div class="w-[10px] h-[10px] bg-[#FF4842] rounded-[50%] absolute top-1.5 right-0"></div>
-                <ul class="sub-nav-notify">
-                    <div class="flex justify-between items-center w-full pb-3 px-3">
-                        <h2 class="text-xl font-normal text-title">Thông báo</h2>
-                        <a href="{{route('ncc_all_noti')}}"
-                           class="hover:text-primary duration-200 transition-all text-title font-medium">Tất
-                            cả</a>
-                    </div>
-                    @if(count(Auth::user()->unreadNotifications) > 0)
-                        @foreach (Auth::user()->unreadNotifications as $index =>$notification)
-                            <li>
-                                <a href="{{$notification['data']['href']}}&noti_id={{$notification->id}}"
-                                   class="flex justify-between items-center w-full text-sm text-title font-bold">{{$notification['data']['message']}}
-                                    <span>{{\Illuminate\Support\Carbon::parse($notification->created_at)->format('h:i A')}} </span></a>
-                            </li>
-                        @endforeach
-                    @else
-                        <div class="text-center"><p>Bạn chưa có thông báo mới nào</p></div>
-                    @endif
-
-
-                </ul>
-            </div>
+            
             <div class="user relative flex items-center gap-2">
+            <div class="flex flex-col gap-[3px] justify-center">
+                    <p class="text-black 2xl:text-base xl:text-sm font-medium cursor-pointer">Xin chào, {{\Illuminate\Support\Facades\Auth::user()->account_code}}!</p>
+                </div>
                 <div class="w-[51px]">
                     <div class="w-[50px] h-[65px] ">
                         <img class="w-full cursor-pointer"
@@ -42,9 +16,7 @@
                 </div>
 
 
-                <div class="flex flex-col gap-[3px] justify-center">
-                    <p class="text-black 2xl:text-base xl:text-sm font-medium cursor-pointer">ID:{{\Illuminate\Support\Facades\Auth::user()->account_code}}</p>
-                </div>
+              
 
                 <ul class="sub-nav-user">
                     <li><a href="{{route('screens.manufacture.account.profile')}}"
@@ -87,6 +59,36 @@
                        </g>
                        </svg>
                             Đăng xuất</a></li>
+                </ul>
+            </div>
+            <div class="notify relative cursor-pointer ">
+                <svg width="32" height="32" viewBox="0 0 32 32" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M13.2841 25.6372C13.9506 25.4962 18.0122 25.4962 18.6788 25.6372C19.2486 25.7688 19.8648 26.0763 19.8648 26.7477C19.8316 27.387 19.4566 27.9536 18.9385 28.3135C18.2666 28.8373 17.4782 29.169 16.6539 29.2885C16.1981 29.3476 15.7502 29.3489 15.3102 29.2885C14.4847 29.169 13.6962 28.8373 13.0257 28.3122C12.5062 27.9536 12.1312 27.387 12.0981 26.7477C12.0981 26.0763 12.7143 25.7688 13.2841 25.6372ZM16.0601 2.66663C18.8337 2.66663 21.6668 3.98265 23.3498 6.16618C24.4417 7.57218 24.9426 8.97683 24.9426 11.1604V11.7284C24.9426 13.403 25.3852 14.39 26.3592 15.5274C27.0973 16.3654 27.3332 17.441 27.3332 18.608C27.3332 19.7736 26.9502 20.8801 26.1829 21.7785C25.1785 22.8555 23.7619 23.5431 22.3162 23.6626C20.2211 23.8412 18.1247 23.9916 16.0005 23.9916C13.875 23.9916 11.7799 23.9016 9.68484 23.6626C8.23778 23.5431 6.8212 22.8555 5.81806 21.7785C5.0508 20.8801 4.6665 19.7736 4.6665 18.608C4.6665 17.441 4.90371 16.3654 5.64049 15.5274C6.64495 14.39 7.0584 13.403 7.0584 11.7284V11.1604C7.0584 8.91775 7.61761 7.45132 8.76916 6.01578C10.4813 3.92222 13.2256 2.66663 15.9409 2.66663H16.0601Z"
+                        fill="#637381"/>
+                </svg>
+                <div class="w-[10px] h-[10px] bg-[#FF4842] rounded-[50%] absolute top-1.5 right-0"></div>
+                <ul class="sub-nav-notify">
+                    <div class="flex justify-between items-center w-full pb-3 px-3">
+                        <h2 class="text-xl font-normal text-title">Thông báo</h2>
+                        <a href="{{route('ncc_all_noti')}}"
+                           class="hover:text-primary duration-200 transition-all text-title font-medium">Tất
+                            cả</a>
+                    </div>
+                    @if(count(Auth::user()->unreadNotifications) > 0)
+                        @foreach (Auth::user()->unreadNotifications as $index =>$notification)
+                            <li>
+                                <a href="{{$notification['data']['href']}}&noti_id={{$notification->id}}"
+                                   class="flex justify-between items-center w-full text-sm text-title font-bold">{{$notification['data']['message']}}
+                                    <span>{{\Illuminate\Support\Carbon::parse($notification->created_at)->format('h:i A')}} </span></a>
+                            </li>
+                        @endforeach
+                    @else
+                        <div class="text-center"><p>Bạn chưa có thông báo mới nào</p></div>
+                    @endif
+
+
                 </ul>
             </div>
         </div>
