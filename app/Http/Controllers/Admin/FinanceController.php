284e39d295452c47e00835e86cd03b57272407be
phongngo2002 <phongcuoi2213@gmail.com>
2023-03-13 10:04:09 +0700

    update export deposit

--- app/Http/Controllers/Admin/FinanceController.php
+++ app/Http/Controllers/Admin/FinanceController.php
@@ -8,6 +8,7 @@ use App\Models\Deposit;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
 
 class FinanceController extends Controller
@@ -21,8 +22,7 @@ class FinanceController extends Controller
 
     public function index()
     {
-        $this->v['histories'] = Deposit::select('name', 'amount', 'id', 'status', 'account_number', 'code', 'old_money', 'bank_id','created_at')
-            ->where('status', 0)
+        $this->v['histories'] = Deposit::select('name', 'amount', 'id', 'status', 'account_number', 'code', 'old_money', 'bank_id', 'created_at')
             ->orderBy('id', 'desc')
             ->paginate(10);
 
@@ -31,6 +31,12 @@ class FinanceController extends Controller
 
     public function exportDeposits(Request $request)
     {
-        return Excel::download(new DepositExport, Carbon::now()->format('d-m-Y') . ' -yeu_cau_rut_tien' . '.xlsx');
+
+        try {
+            return Excel::download(new DepositExport($request->start_date, $request->end_date), Carbon::now()->format('d-m-Y') . ' -yeu_cau_rut_tien' . '.xlsx');
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+            return redirect()->back()->with('success', 'Có lỗi xảy ra.Vui lòng thử lại');
+        }
     }
 }
