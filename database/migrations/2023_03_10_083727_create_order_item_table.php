a802868514c5146e1e18fce91850cc16b533cc38
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-03 10:24:29 +0700

    update api product của kho

--- database/migrations/2023_03_10_083727_create_order_item_table.php
+++ database/migrations/2023_03_10_083727_create_order_item_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -25,11 +24,11 @@ return new class extends Migration
             $table->integer('status')->default(0);
             $table->integer('warehouses_id');
             $table->integer('discount');
-            $table->float('discount_vshop',8,2);
-            $table->float('discount_ncc',8,2);
-            $table->float('discount_vstore',8,2);
+            $table->float('discount_vshop', 8, 2);
+            $table->float('discount_ncc', 8, 2);
+            $table->float('discount_vstore', 8, 2);
             $table->integer('ncc_id')->default(0);
-
+            $table->integer('request_warehouse_id')->default(0);
             $table->timestamps();
         });
     }
