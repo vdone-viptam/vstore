fbdba48c4075fe267687e887ea0bcace634128e8
devNgoLong <dev.ngovanlong@gmail.com>
2023-04-03 16:32:05 +0700

    Push els

---
+++ config/elasticsearch.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return [
+    'node' => env('ELASTICSEARCH_NODE', 'localhost:9200'),
+    'username' => env('ELASTICSEARCH_USERNAME', ''),
+    'password' => env('ELASTICSEARCH_PASSWORD', ''),
+    'max_retries' => env('ELASTICSEARCH_MAX_RETRIES', ''),
+    'req_timeout' => env('ELASTICSEARCH_REQ_TIMEOUT', ''),
+    'vstore_products' => 'vstore_products'
+];
