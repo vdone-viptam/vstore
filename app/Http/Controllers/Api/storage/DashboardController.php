07659e43274bb2efdef75b4431aa320d8fc8c41a
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-05 17:01:08 +0700

    update api product

--- app/Http/Controllers/Api/storage/DashboardController.php
+++ app/Http/Controllers/Api/storage/DashboardController.php
@@ -74,9 +74,9 @@ class DashboardController extends Controller
                 'products.sku_id'
             );
         $products = $products->join('request_warehouses', 'products.id', '=', 'request_warehouses.product_id')
-            ->whereIn('request_warehouses.type', [1, 10, 2, 7])
+            ->whereIn('request_warehouses.type', [1, 10, 2, 3])
             ->where('request_warehouses.ware_id', $warehouses->id)
-            ->where('request_warehouses.status', 0)
+            ->whereIn('request_warehouses.status', [7, 0])
             ->orderBy('request_warehouses.id', 'desc')
             ->limit($request->limit ?? 5)->get();
 
