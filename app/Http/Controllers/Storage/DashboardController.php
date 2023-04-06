f9709b030ad36902a8faa9c5a1c156179aa6d8d7
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-06 11:29:33 +0700

    update api product

--- app/Http/Controllers/Storage/DashboardController.php
+++ app/Http/Controllers/Storage/DashboardController.php
@@ -3,16 +3,85 @@
 namespace App\Http\Controllers\Storage;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use App\Models\User;
+use App\Models\Warehouses;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     //
-
-    public function index()
+    public function index(Request $request)
     {
-        return view('screens.storage.dashboard.index', []);
+        $warehouses = Warehouses::where('user_id', Auth::id())->first();
+        $publish_id = $request->publish_id;
+        $requestEx = Product::join('order_item', 'products.id', '=', 'order_item.product_id')
+            ->join('order', 'order_item.order_id', '=', 'order.id')
+            ->select(
+                'order.no',
+                'products.publish_id',
+                'products.name',
+                'order_item.quantity',
+                'order.method_payment',
+                'order.export_status',
+                'order.created_at',
+                'order.id'
+            );
+
+        $requestEx = $requestEx->where('order.status', '!=', 2)
+            ->where('export_status', 0)
+            ->where('order_item.warehouse_id', $warehouses->id)
+            ->count();
+        $requestIm = User::join('products', 'users.id', '=', 'products.user_id')
+            ->select('request_warehouses.code',
+                'products.publish_id',
+                'products.name as product_name',
+                'users.name as ncc_name',
+                'quantity',
+                'request_warehouses.created_at',
+                'request_warehouses.status',
+                'request_warehouses.id'
+            )
+            ->join('request_warehouses', 'products.id', '=', 'request_warehouses.product_id')
+            ->where('request_warehouses.status', 0)
+            ->where('type', 1)
+            ->where('request_warehouses.ware_id', $warehouses->id)
+            ->count();
+        $productOutStock = count(DB::select(DB::raw("
+        SELECT amount - export - (SELECT IFNULL(SUM(quantity),0)
+                                  FROM request_warehouses WHERE `status` = 0
+                                                            AND type = 2 AND ware_id = $warehouses->id
+                                                            AND product_id = product_warehouses.product_id) as 'total'
+        FROM product_warehouses
+        WHERE ware_id = $warehouses->id HAVING total <= 10
+        "))) ?? 0;
+
+        $products = User::join('products', 'users.id', '=', 'products.user_id')
+            ->select(
+                'request_warehouses.code',
+                'products.publish_id',
+                'products.name as product_name',
+                'users.name as ncc_name',
+                'quantity',
+                'request_warehouses.status',
+                'request_warehouses.created_at',
+                'request_warehouses.type',
+                'request_warehouses.id',
+                'products.sku_id'
+            );
+        $products = $products->join('request_warehouses', 'products.id', '=', 'request_warehouses.product_id')
+            ->whereIn('request_warehouses.type', [1, 10, 2, 3])
+            ->where('request_warehouses.ware_id', $warehouses->id)
+            ->whereIn('request_warehouses.status', [7, 0])
+            ->orderBy('request_warehouses.id', 'desc')
+            ->paginate($request->limit ?? 10);
+        return view('screens.storage.dashboard.index', [
+            'success' => true,
+            'requestEx' => $requestEx,
+            'requestIm' => $requestIm,
+            'productOutStock' => $productOutStock,
+            'products' => $products]);
     }
 }
---
+++ resources/views/layouts/storage/siderbar.blade.php
@@ -0,0 +1,115 @@
+<div class="nav-left-sidebar sidebar-dark">
+    <div class="menu-list">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="d-xl-none d-lg-none" href="#"> Trang chủ</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav flex-column">
+                    <li class="nav-divider">
+                        Menu
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link active" href="./index.html" aria-expanded="false"
+                           data-target="#submenu-1" aria-controls="submenu-1"><i
+                                class="fa fa-fw fas fa-home"></i>Tổng quan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false"
+                           data-target="#submenu-2" aria-controls="submenu-2"><i
+                                class="fa fa-fw fas fa-truck"></i>Hàng hóa
+                        </a>
+                        <div id="submenu-2" class="collapse submenu" style="">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./product.html">Tất cả sản phẩm</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./request.html">Quản lý yêu cầu nhập kho</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./requestAcp.html">Xác nhận đơn hàng</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false"
+                           data-target="#submenu-3" aria-controls="submenu-3"><i
+                                class="fas fa-fw fas fa-warehouse"></i>Quản lý kho</a>
+                        <div id="submenu-3" class="collapse submenu" style="">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./proIn.html">Nhập hàng</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./proOut.html">Xuất hàng</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./proDel.html">Xuất hủy</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./listDel.html">Đơn hàng hủy</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false"
+                           data-target="#submenu-4" aria-controls="submenu-4"><i
+                                class="fab fa-fw fas fa-users"></i>Đối tác</a>
+                        <div id="submenu-4" class="collapse submenu" style="">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./listSupplier.html">Nhà cung cấp</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./listShip.html">Đối tác giao hàng</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false"
+                           data-target="#submenu-6" aria-controls="submenu-6"><i
+                                class="fas fa-fw fa-dollar-sign"></i>
+                            Tài chính</a>
+                        <div id="submenu-6" class="collapse submenu" style="">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./finance.html">Ví</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./history.html">Lịch sử thay đổi số
+                                        dư</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false"
+                           data-target="#submenu-7" aria-controls="submenu-7"><i
+                                class="fa fa-fw fa-user-circle"></i>Tài khoản</a>
+                        <div id="submenu-7" class="collapse submenu" style="">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./profile.html">Hồ sơ của tôi</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./change-password.html">Đổi mật khẩu</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link logout" aria-expanded="false" data-target="#submenu-8"
+                           aria-controls="submenu-8" style="color:#FF4D4F"><i class="fas fa-fw fa-sign-out-alt"
+                                                                              style="color:#FF4D4F"></i>Đăng xuất</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
