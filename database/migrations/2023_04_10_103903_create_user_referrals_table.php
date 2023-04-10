bd3a1f75c5c225f9a600d9aa9a54e4f2a44cf8ef
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-10 11:00:22 +0700

    update api product

--- database/migrations/2023_04_10_103903_create_user_referrals_table.php
+++ database/migrations/2023_04_10_103903_create_user_referrals_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::create('user_referrals', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
-            $table->integer('vshop_id');
+            $table->string('vshop_id');
             $table->timestamps();
         });
     }
