c98a127b52deddeeed7de297f926af39bdfca5fb
xuan hiep <xuanhiep284@gmail.com>
2023-04-10 10:17:09 +0700

    update kho

--- app/Interfaces/Storage/Partner/PartnerRepositoryInterface.php
+++ app/Interfaces/Storage/Partner/PartnerRepositoryInterface.php
@@ -6,6 +6,6 @@ interface PartnerRepositoryInterface
 {
     public function index($search , $limit);
     public function detailNcc($user_id);
-    public function deliveryPartner($limit);
+    public function deliveryPartner($search, $limit);
     public function detailDeliveryPartner($delivery_partner_id);
 }
