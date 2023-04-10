702ed7244a04da01f7370e33e90078a9cb58bd30
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-10 10:20:53 +0700

    update api product

--- app/Http/Controllers/Storage/PartnerController.php
+++ app/Http/Controllers/Storage/PartnerController.php
@@ -10,29 +10,34 @@ use Illuminate\Http\Request;
 class PartnerController extends Controller
 {
     private PartnerRepositoryInterface $partnerRepository;
+
     public function __construct(PartnerRepositoryInterface $partnerRepository)
     {
         $this->partnerRepository = $partnerRepository;
     }
+
     public function index(Request $request)
     {
         $search = $request->key_search;
-        $limit = $request->limit ?? 10 ;
-        $suppliers = $this->partnerRepository->index($search,$limit);
-        return view('screens.storage.partner.supplier.index', ['suppliers' => $suppliers]);
+        $limit = $request->limit ?? 10;
+        $suppliers = $this->partnerRepository->index($search, $limit);
+        return view('screens.storage.partner.supplier.index', ['suppliers' => $suppliers, 'key_search' => trim($search)]);
     }
+
     public function detailNcc(Request $request)
     {
         $ncc = $this->partnerRepository->detailNcc($request->user_id);
         return response()->json(['success' => true, 'data' => $ncc]);
     }
+
     public function deliveryPartner(Request $request)
     {
         $search = $request->key_search;
         $limit = $request->limit ?? 10;
         $deliveryPartners = $this->partnerRepository->deliveryPartner($search, $limit);
-        return view('screens.storage.partner.delivery-partner.index',['deliveryPartners' => $deliveryPartners]);
+        return view('screens.storage.partner.delivery-partner.index', ['deliveryPartners' => $deliveryPartners, 'key_search' => trim($search)]);
     }
+
     public function detailDeliveryPartner(Request $request)
     {
         $deliveryPartners = $this->partnerRepository->detailDeliveryPartner($request->delivery_partner_id);
