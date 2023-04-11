9558880e4466e319c4c998d795c019e691c68228
ducanh6629417 <ducanh01273635868@gmail.com>
2023-04-11 16:22:22 +0700

    add

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -63,7 +63,7 @@ class ProductController extends Controller
             }
             $products = $products->select($selected);
             if ($request->category_id) {
-                $products = $products->where('category_id', $request->category_id);
+                $products = $products->where('category_id', $request->category_id)->get();
             }
             if ($request->order_by == 1) {
 
@@ -690,7 +690,7 @@ class ProductController extends Controller
         $cate = [];
 
         $products = DB::table('vshop')
-            ->select('products.name', 'publish_id', 'price', 'images', 'products.id', 'discount_vShop as discount_vstore', 'categories.name as cate_name')
+            ->select('products.name', 'publish_id', 'price', 'images', 'products.id', 'discount_vShop as discountVstore', 'categories.name as cate_name')
             ->join('vshop_products', 'vshop.id', '=', 'vshop_products.vshop_id')
             ->join('products', 'vshop_products.product_id', '=', 'products.id')
             ->join('categories', 'products.category_id', '=', 'categories.id')
