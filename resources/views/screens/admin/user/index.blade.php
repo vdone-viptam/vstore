5dc64dca649754da4c820a310b51e309c01cc57c
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-11 21:01:40 +0700

    update order by request out

--- resources/views/screens/admin/user/index.blade.php
+++ resources/views/screens/admin/user/index.blade.php
@@ -36,7 +36,7 @@
                        class="outline-none rounded-xl border-[1px] border-[#EBEBEB] px-4 py-[5px] focus:border-primary transition-all duration-200"
                        placeholder="Tìm kiếm">
                 <button type="submit" id="btnSearch"
-                        class="flex items-center gap-2 cursor-pointer transition-all duration-200 hover:opacity-70 rounded-xl outline-none border-[1px] bg-[#40BAFF] text-[#FFF] px-4 py-[5px] "
+                        class="btnA flex items-center gap-2 cursor-pointer transition-all duration-200 hover:opacity-70 rounded-xl outline-none border-[1px] bg-[#40BAFF] text-[#FFF] px-4 py-[5px] "
                 >
 
                     Tìm kiếm
