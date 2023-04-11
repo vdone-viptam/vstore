67db430e51cbef612d0b1037afeb5269cb3b9047
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-11 15:36:28 +0700

    update order by request out

--- app/Exports/DepositExport.php
+++ app/Exports/DepositExport.php
@@ -15,36 +15,19 @@ class DepositExport implements FromCollection, WithHeadings
 
     private $end_date = null;
 
-    public function __construct($start_date, $end_date)
+    public function __construct($limit, $offset)
     {
-        $this->start_date = $start_date;
-        $this->end_date = $end_date;
+        $this->limit = $limit;
+        $this->offset = $offset;
     }
 
     public function collection()
     {
-        $query = Deposit::select('code as Mã giao dịch', 'deposits.name as Chủ tài khoản', 'amount as Số tiền rút', 'account_number as Số tài khoản', 'banks.name as Tên ngân hàng', 'full_name as Tên đầy đủ ngân hàng', 'deposits.created_at as Ngày yêu cầu')
+        $query = Deposit::query()->select('code as Mã giao dịch', 'deposits.name as Chủ tài khoản', 'amount as Số tiền rút', 'account_number as Số tài khoản', 'banks.name as Tên ngân hàng', 'full_name as Tên đầy đủ ngân hàng', 'deposits.created_at as Ngày yêu cầu')
             ->join('banks', 'deposits.bank_id', '=', 'banks.id')
-            ->where('status', 0);
-
-        if ($this->start_date != null) {
-
-            $query = $query->where('deposits.created_at', '>=', $this->start_date);
-        }
-        if ($this->end_date != null) {
-            $query = $query->where('deposits.created_at', '<=', $this->end_date);
-        }
-
-        $query = $query->get();
-        $query1 = Deposit::where('status', 0);
-        if ($this->start_date != null) {
-            $query1 = $query1->where('created_at', '>=', $this->start_date);
-        }
-        if ($this->end_date != null) {
-            $query1 = $query1->where('created_at', '<=', $this->end_date);
-        }
-
-        $query1->update(['status' => 1]);
+            ->limit($this->limit)
+            ->offset($this->offset)
+            ->get();
         return $query;
 
     }
