40de77095995d83fe2a785a149d6e3d1dd6790f8
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-13 18:12:56 +0700

    update order by request out

--- resources/views/screens/manufacture/finance/index.blade.php
+++ resources/views/screens/manufacture/finance/index.blade.php
@@ -350,9 +350,9 @@
 
         document.getElementsByName('name').forEach(item => {
             item.addEventListener("keypress", (e) => {
-                var regex = new RegExp("^[0-9]+$");
+                var regex = new RegExp("^[A-Z]+$");
                 var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
-                if (regex.test(key)) {
+                if (!regex.test(key)) {
                     event.preventDefault();
                     return false;
                 }
