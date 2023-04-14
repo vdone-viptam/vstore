72fa596c227261252c16d87bf999a0a51c1d33cc
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-14 17:40:37 +0700

    update order by request out

--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -2,6 +2,7 @@
         integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="{{asset('asset/js/main.js')}}"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"
         integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
