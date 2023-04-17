55b16f177ab4b80df5cab9f53b8b0207ebeb5cd5
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-17 17:36:55 +0700

    update order by request out

--- resources/views/layouts/vstore/main.blade.php
+++ resources/views/layouts/vstore/main.blade.php
@@ -175,11 +175,12 @@
         });
     }
     const x = document.querySelectorAll('input[type="number"]');
-    x.addEventListener("keypress", function (evt) {
-        if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57)
-        {
-            evt.preventDefault();
-        }
-    });
+    x.forEach(item => {
+        item.addEventListener("keypress", function (evt) {
+            if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57) {
+                evt.preventDefault();
+            }
+        });
+    })
 </script>
 </html>
