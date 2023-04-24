964b8a59491decd2f32f2e96ffb157ddf83dea5c
xuan hiep <xuanhiep284@gmail.com>
2023-04-24 16:00:21 +0700

    pending upload partner

--- app/Http/Controllers/Manufacture/PartnerController.php
+++ app/Http/Controllers/Manufacture/PartnerController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PartnerController extends Controller
 {
@@ -16,22 +17,41 @@ class PartnerController extends Controller
 
     public function index(Request $request)
     {
+        // dd(Auth::id());
         $this->v['field'] = $request->field ?? 'products.id';
         $this->v['type'] = $request->type ?? 'desc';
         $key_search = $request->key_search ?? '';
         $this->v['products'] = Product::join('users', 'products.vstore_id', '=', 'users.id')
             ->where('products.user_id', Auth::id())
             ->where('products.status', 2);
-        if ($key_search && strlen(($key_search) > 0)) {
-            $this->v['products'] = $this->v['products']->where(function ($sub) use ($key_search) {
-                $sub->where('products.name', 'like', '%' . $key_search . '%')
-                    ->orWhere('products.publish_id', 'like', '%' . $key_search . '%');
-            });
-        }
-        $this->v['products'] = $this->v['products']->select('products.publish_id', 'products.name as name', 'products.price', 'users.name as vstore_name', 'products.discount', 'products.amount_product_sold')
+        // if ($key_search && strlen(($key_search) > 0)) {
+        //     $this->v['products'] = $this->v['products']->where(function ($sub) use ($key_search) {
+        //         $sub->where('products.name', 'like', '%' . $key_search . '%')
+        //             ->orWhere('products.publish_id', 'like', '%' . $key_search . '%');
+        //     });
+        // }
+        $this->v['products'] = $this->v['products']
+            ->select('users.name as vstore_name','users.account_code','users.phone_number','vstore_id',
+                    DB::raw('COUNT(products.id) as total_product'),
+                )
+            // ->selectSub('select count(id) from categories
+            //             where id = products.category_id GROUP BY id',
+            //             'total_category')
+            ->groupBy('vstore_name','users.account_code','users.phone_number')
             ->orderBy($this->v['field'], $this->v['type'])
             ->paginate($request->limit ?? 10);
 
+            foreach($this->v['products'] as $d){
+                $countCategory = Product::where('products.user_id', Auth::id())
+                                ->where('products.vstore_id',$d->vstore_id)
+                                ->where('products.status', 2)
+                                ->select(
+                                    DB::raw('COUNT(products.category_id) as total_category'),
+                                    'products.category_id'
+                                )->groupBy('category_id')
+                                ->get()->toArray();
+                $d->total_category = count($countCategory);
+            }
         $this->v['key_search'] = $request->key_search ?? '';
         $this->v['limit'] = $request->limit ?? 10;
         $this->v['params'] = $request->all();
@@ -41,6 +61,16 @@ class PartnerController extends Controller
     public function report()
     {
         return view('screens.manufacture.partner.report', []);
+
+    }
+    public function detail(Request $request)
+    {
+        $data = Product::where('products.user_id', Auth::id())
+                ->where('products.vstore_id',$request->vstore_id)
+                ->where('products.status', 2)
+                ->select('discount','price')
+                ->get();
+        dd($data);
 
     }
 }
