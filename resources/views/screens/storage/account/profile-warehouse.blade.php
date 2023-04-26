f3b73af2bbb2e6b249d2eb6f8c829bc96764e252
xuan hiep <xuanhiep284@gmail.com>
2023-04-26 20:22:33 +0700

    update

--- resources/views/screens/storage/account/profile-warehouse.blade.php
+++ resources/views/screens/storage/account/profile-warehouse.blade.php
@@ -73,7 +73,14 @@ position: relative;
 
 @endsection
 @section('modal')
+<div class="modal fade" id="modalDetail" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+aria-hidden="true">
+<div class="modal-dialog" role="document">
+    <div class="img-zoom-full w-100 h-100">
 
+    </div>
+</div>
+</div>
 @endsection
 @section('content')
     <div class="container-fluid dashboard-content ">
@@ -169,7 +176,7 @@ position: relative;
 
                                                             </div>
                                                             <input type="hidden" class="input-image" name="normalImageStorage">
-                                                            <div class=" add-img-SP add-imgSP">
+                                                            <div class=" add-img-SP add-imgSP" data-arrImg="1">
                                                                 <svg width="14" height="14" class="cursor-pointer" viewBox="0 0 14 14" fill="none"
                                                                         xmlns="http://www.w3.org/2000/svg">
                                                                     <path
@@ -189,11 +196,11 @@ position: relative;
                                                     <label style="font-weight: 600;" for="tts">Giấy chứng nhận
                                                         PCCC/Chứng nhận khác</label>
                                                         <div class="d-flex align-items-start flex-wrap" style="gap: 16px;">
-                                                            <div class="file-sp img-cn">
+                                                            <div class="file-sp img-sp">
 
                                                             </div>
                                                             <input type="hidden" class="input-image" name="normalImagePccc">
-                                                            <div class=" add-img-SP add-imgCN">
+                                                            <div class=" add-img-SP add-imgSP" data-arrImg="2">
                                                                 <svg width="14" height="14" class="cursor-pointer" viewBox="0 0 14 14" fill="none"
                                                                         xmlns="http://www.w3.org/2000/svg">
                                                                     <path
@@ -285,7 +292,7 @@ position: relative;
 
                                                         </div>
                                                         <input type="hidden" class="input-image" name="coldImageStorage">
-                                                        <div class=" add-img-SP add-imgSP">
+                                                        <div class=" add-img-SP add-imgSP" data-arrImg="3">
                                                             <svg width="14" height="14" class="cursor-pointer" viewBox="0 0 14 14" fill="none"
                                                                     xmlns="http://www.w3.org/2000/svg">
                                                                 <path
@@ -305,11 +312,11 @@ position: relative;
                                                 <label style="font-weight: 600;" for="tts">Giấy chứng nhận
                                                     PCCC/Chứng nhận khác</label>
                                                     <div class="d-flex align-items-start flex-wrap" style="gap: 16px;">
-                                                        <div class="file-sp img-cn">
+                                                        <div class="file-sp img-sp">
 
                                                         </div>
                                                         <input type="hidden" class="input-image" name="coldImagePccc">
-                                                        <div class=" add-img-SP add-imgCN">
+                                                        <div class=" add-img-SP add-imgSP" data-arrImg="4">
                                                             <svg width="14" height="14" class="cursor-pointer" viewBox="0 0 14 14" fill="none"
                                                                     xmlns="http://www.w3.org/2000/svg">
                                                                 <path
@@ -400,7 +407,7 @@ position: relative;
 
                                                         </div>
                                                         <input type="hidden" class="input-image" name="warehouseImageStorage">
-                                                        <div class=" add-img-SP add-imgSP">
+                                                        <div class=" add-img-SP add-imgSP" data-arrImg="5">
                                                             <svg width="14" height="14" class="cursor-pointer" viewBox="0 0 14 14" fill="none"
                                                                     xmlns="http://www.w3.org/2000/svg">
                                                                 <path
@@ -420,11 +427,11 @@ position: relative;
                                                 <label style="font-weight: 600;" for="tts">Giấy chứng nhận
                                                     PCCC/Chứng nhận khác</label>
                                                     <div class="d-flex align-items-start flex-wrap" style="gap: 16px;">
-                                                        <div class="file-sp img-cn">
+                                                        <div class="file-sp img-sp">
 
                                                         </div>
                                                         <input type="hidden" class="input-image" name="warehouseImagePccc">
-                                                        <div class=" add-img-SP add-imgCN">
+                                                        <div class=" add-img-SP add-imgSP" data-arrImg="6">
                                                             <svg width="14" height="14" class="cursor-pointer" viewBox="0 0 14 14" fill="none"
                                                                     xmlns="http://www.w3.org/2000/svg">
                                                                 <path
@@ -462,8 +469,14 @@ position: relative;
 
 let i = 1;
         let arrImage = [];
-        let arrUnit = [];
+        let arrImage1 = [];
+        let arrImage2 = [];
+        let arrImage3 = [];
+        let arrImage4 = [];
+        let arrImage5 = [];
+        let arrImage6 = [];
 $('.add-imgSP').on('click', function () {
+    // console.log($(this).attr('data-arrImg'));
   let input = document.createElement('input');
   input.type = 'file';
   input.onchange = _ => {
@@ -482,7 +495,7 @@ $('.add-imgSP').on('click', function () {
     <path d="M15.6822 7.5404C13.9894 3.97433 11.4305 2.17969 8.0001 2.17969C4.56796 2.17969 2.01082 3.97433 0.31796 7.54219C0.250059 7.68597 0.214844 7.843 0.214844 8.00201C0.214844 8.16102 0.250059 8.31805 0.31796 8.46183C2.01082 12.0279 4.56975 13.8225 8.0001 13.8225C11.4322 13.8225 13.9894 12.0279 15.6822 8.46005C15.8197 8.17076 15.8197 7.83505 15.6822 7.5404V7.5404ZM8.0001 12.5368C5.11975 12.5368 3.01082 11.0761 1.52332 8.00112C3.01082 4.92612 5.11975 3.4654 8.0001 3.4654C10.8805 3.4654 12.9894 4.92612 14.4769 8.00112C12.9912 11.0761 10.8822 12.5368 8.0001 12.5368ZM7.92867 4.85826C6.19296 4.85826 4.78582 6.2654 4.78582 8.00112C4.78582 9.73683 6.19296 11.144 7.92867 11.144C9.66439 11.144 11.0715 9.73683 11.0715 8.00112C11.0715 6.2654 9.66439 4.85826 7.92867 4.85826ZM7.92867 10.0011C6.82332 10.0011 5.92867 9.10647 5.92867 8.00112C5.92867 6.89576 6.82332 6.00112 7.92867 6.00112C9.03403 6.00112 9.92868 6.89576 9.92868 8.00112C9.92868 9.10647 9.03403 10.0011 7.92867 10.0011Z" fill="white" ></path>
 
     </svg>
-              <svg width="16" height="16"  style="cursor-pointer" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg class="delete-one-image" width="16" height="16"  style="cursor-pointer" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M5.28544 2.14118H5.14258C5.22115 2.14118 5.28544 2.0769 5.28544 1.99833V2.14118H10.714V1.99833C10.714 2.0769 10.7783 2.14118 10.8569 2.14118H10.714V3.4269H11.9997V1.99833C11.9997 1.36797 11.4872 0.855469 10.8569 0.855469H5.14258C4.51222 0.855469 3.99972 1.36797 3.99972 1.99833V3.4269H5.28544V2.14118ZM14.2854 3.4269H1.71401C1.39794 3.4269 1.14258 3.68225 1.14258 3.99833V4.56975C1.14258 4.64833 1.20686 4.71261 1.28544 4.71261H2.36401L2.80508 14.0519C2.83365 14.6608 3.33722 15.1412 3.94615 15.1412H12.0533C12.664 15.1412 13.1658 14.6626 13.1944 14.0519L13.6354 4.71261H14.714C14.7926 4.71261 14.8569 4.64833 14.8569 4.56975V3.99833C14.8569 3.68225 14.6015 3.4269 14.2854 3.4269ZM11.9158 13.8555H4.08365L3.65151 4.71261H12.3479L11.9158 13.8555Z" fill="white"/>
               </svg>
               </div>
@@ -542,11 +555,18 @@ function zoomImg(img){
 function render(data, elementInput) {
 
     const html = data.map((item, index) => {
-        return `<div class="item w-[104px] h-[104px] flex justify-center items-center relative">
-            <div class="over-lay"></div>
-            <svg width="16" height="16" data-index="${index}" class="deleteImg cursor-pointer" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M5.28544 2.14118H5.14258C5.22115 2.14118 5.28544 2.0769 5.28544 1.99833V2.14118H10.714V1.99833C10.714 2.0769 10.7783 2.14118 10.8569 2.14118H10.714V3.4269H11.9997V1.99833C11.9997 1.36797 11.4872 0.855469 10.8569 0.855469H5.14258C4.51222 0.855469 3.99972 1.36797 3.99972 1.99833V3.4269H5.28544V2.14118ZM14.2854 3.4269H1.71401C1.39794 3.4269 1.14258 3.68225 1.14258 3.99833V4.56975C1.14258 4.64833 1.20686 4.71261 1.28544 4.71261H2.36401L2.80508 14.0519C2.83365 14.6608 3.33722 15.1412 3.94615 15.1412H12.0533C12.664 15.1412 13.1658 14.6626 13.1944 14.0519L13.6354 4.71261H14.714C14.7926 4.71261 14.8569 4.64833 14.8569 4.56975V3.99833C14.8569 3.68225 14.6015 3.4269 14.2854 3.4269ZM11.9158 13.8555H4.08365L3.65151 4.71261H12.3479L11.9158 13.8555Z" fill="white"/>
-        </svg>
+        return `<div class="item" key=${index}>
+            <div class="over-lay-img"></div>
+              <div class="d-flex justify-content-center align-items-center deleteImg" style="gap:10px;">
+              <svg width="16" height="16" class="zoom-img" onclick="zoomImg('${item}')" style="cursor-pointer" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M15.6822 7.5404C13.9894 3.97433 11.4305 2.17969 8.0001 2.17969C4.56796 2.17969 2.01082 3.97433 0.31796 7.54219C0.250059 7.68597 0.214844 7.843 0.214844 8.00201C0.214844 8.16102 0.250059 8.31805 0.31796 8.46183C2.01082 12.0279 4.56975 13.8225 8.0001 13.8225C11.4322 13.8225 13.9894 12.0279 15.6822 8.46005C15.8197 8.17076 15.8197 7.83505 15.6822 7.5404V7.5404ZM8.0001 12.5368C5.11975 12.5368 3.01082 11.0761 1.52332 8.00112C3.01082 4.92612 5.11975 3.4654 8.0001 3.4654C10.8805 3.4654 12.9894 4.92612 14.4769 8.00112C12.9912 11.0761 10.8822 12.5368 8.0001 12.5368ZM7.92867 4.85826C6.19296 4.85826 4.78582 6.2654 4.78582 8.00112C4.78582 9.73683 6.19296 11.144 7.92867 11.144C9.66439 11.144 11.0715 9.73683 11.0715 8.00112C11.0715 6.2654 9.66439 4.85826 7.92867 4.85826ZM7.92867 10.0011C6.82332 10.0011 5.92867 9.10647 5.92867 8.00112C5.92867 6.89576 6.82332 6.00112 7.92867 6.00112C9.03403 6.00112 9.92868 6.89576 9.92868 8.00112C9.92868 9.10647 9.03403 10.0011 7.92867 10.0011Z" fill="white" ></path>
+
+    </svg>
+              <svg class="delete-one-image" width="16" height="16"  style="cursor-pointer" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5.28544 2.14118H5.14258C5.22115 2.14118 5.28544 2.0769 5.28544 1.99833V2.14118H10.714V1.99833C10.714 2.0769 10.7783 2.14118 10.8569 2.14118H10.714V3.4269H11.9997V1.99833C11.9997 1.36797 11.4872 0.855469 10.8569 0.855469H5.14258C4.51222 0.855469 3.99972 1.36797 3.99972 1.99833V3.4269H5.28544V2.14118ZM14.2854 3.4269H1.71401C1.39794 3.4269 1.14258 3.68225 1.14258 3.99833V4.56975C1.14258 4.64833 1.20686 4.71261 1.28544 4.71261H2.36401L2.80508 14.0519C2.83365 14.6608 3.33722 15.1412 3.94615 15.1412H12.0533C12.664 15.1412 13.1658 14.6626 13.1944 14.0519L13.6354 4.71261H14.714C14.7926 4.71261 14.8569 4.64833 14.8569 4.56975V3.99833C14.8569 3.68225 14.6015 3.4269 14.2854 3.4269ZM11.9158 13.8555H4.08365L3.65151 4.71261H12.3479L11.9158 13.8555Z" fill="white"/>
+              </svg>
+              </div>
+
         <img src="${item}" class="w-100"></img></div>`;
     }).join("")
 
@@ -563,7 +583,7 @@ function render(data, elementInput) {
     // document.getElementById('images').value = JSON.stringify(data);
 
 
-    document.querySelectorAll('.deleteImg').forEach(item => {
+    document.querySelectorAll('.delete-one-image').forEach(item => {
         const {index} = item.dataset;
         item.addEventListener('click', () => {
             arrImage = data.filter((item1, index1) => index1 !== +index);
