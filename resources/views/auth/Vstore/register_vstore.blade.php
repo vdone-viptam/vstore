ebf5b17c7aa583c3c638aea7afdd7fae9a6fcd02
Hoangngochieptt <phangiangxinh55@gmail.com>
2023-04-26 16:10:28 +0700

    up

--- resources/views/auth/Vstore/register_vstore.blade.php
+++ resources/views/auth/Vstore/register_vstore.blade.php
@@ -241,10 +241,8 @@
                 </div>
                 <div class="form-group" style="max-width: 450px; margin: 0 auto;">
                     <label class="custom-control custom-checkbox">
-                        <input class="custom-control-input" type="checkbox" value="1" required=""><span
-                            class="custom-control-label"> Tôi đồng ý với <a
-                                href="#" onclick="$('.modal-hd').toggleClass('show-modal')"
-                                class="underline text-blue-700">Điều khoản sử dụng dịch vụ</a>. </span>
+                        <input class="custom-control-input" required type="checkbox"><span class="custom-control-label">Tôi đồng
+                            ý với <span style="text-decoration: underline;cursor: pointer;"  class="underline text-blue-700" data-toggle="modal" data-target=".modal-terms">Điều khoản sử dụng dịch vụ.</span></span>
                     </label>
                     <div class="form-group pt-2">
                         <button type="submit" class="active btn btn-block btn-primary">Đăng ký mua</button>
@@ -258,7 +256,56 @@
             </div>
         </div>
     </form>
+{{-- modal terms --}}
+<div class="modal fade modal-terms" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
 
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" style="font-size: 20px;">Điều khoản sử dụng</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="content col-12 my-4">
+                VDONE tạo ra các công nghệ và dịch vụ nhằm hỗ trợ mọi người kết nối với nhau, xây
+                dựng cộng đồng cũng như phát triển doanh nghiệp. Các Điều khoản này điều chỉnh việc
+                bạn sử dụng các sản phẩm, tính năng, ứng dụng, dịch vụ, công nghệ cũng như phần mềm
+                khác mà chúng tôi cung cấp (Sản phẩm của VDONE), trừ khi chúng tôi nêu rõ là áp dụng
+                các điều khoản riêng (và không áp dụng các điều khoản này). Các Sản phẩm này do
+                VipTam, Inc. cung cấp cho bạn.
+                Bạn không mất phí sử dụng các sản phẩm và dịch vụ khác thuộc phạm vi điều chỉnh của
+                những Điều khoản này, trừ khi chúng tôi có quy định khác. Thay vào đó, doanh nghiệp,
+                tổ chức và những cá nhân khác sẽ phải trả tiền cho chúng tôi để hiển thị quảng cáo
+                về sản phẩm và dịch vụ của họ cho bạn. Khi sử dụng Sản phẩm của chúng tôi, bạn đồng
+                ý để chúng tôi hiển thị quảng cáo mà chúng tôi cho rằng có thể phù hợp với bạn và sở
+                thích của bạn. Chúng tôi sử dụng dữ liệu cá nhân của bạn để xác định những quảng cáo
+                được cá nhân hóa sẽ hiển thị cho bạn.
+                Chúng tôi không bán dữ liệu cá nhân của bạn cho các nhà quảng cáo, cũng không chia
+                sẻ thông tin trực tiếp nhận dạng bạn (chẳng hạn như tên, địa chỉ email hoặc thông
+                tin liên hệ khác) với những đơn vị này trừ khi được bạn cho phép cụ thể. Thay vào
+                đó, các nhà quảng cáo có thể cho chúng tôi biết thông tin như kiểu đối tượng mà họ
+                muốn nhìn thấy quảng cáo và chúng tôi có thể hiển thị những quảng cáo ấy cho người
+                có thể quan tâm.
+                Chúng tôi cho nhà quảng cáo biết hiệu quả quảng cáo để những đơn vị này nắm được
+                cách mọi người tương tác với nội dung của họ. Hãy xem Mục 2 ở bên dưới để hiểu rõ
+                hơn cách chúng tôi hiển thị quảng cáo được cá nhân hóa trên Sản phẩm của VDONE theo
+                các điều khoản này. Chính sách quyền riêng tư của chúng tôi giải thích cách chúng
+                tôi thu thập và sử dụng dữ liệu cá nhân của bạn để quyết định hiển thị cho bạn quảng
+                cáo nào, cũng như để cung cấp tất cả các dịch vụ khác được mô tả bên dưới. Bạn cũng
+                có thể chuyển đến trang cài đặt trên Sản phẩm có liên quan của VDONE bất cứ lúc nào
+                để xem lại các lựa chọn quyền riêng tư mình có đối với cách chúng tôi sử dụng dữ
+                liệu của bạn.
+            </div>
+            <div class="modal-footer">
+
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng
+                    lại</button>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- modal terms --}}
 
     <script src="{{ asset('asset/js/main.js') }}"></script>
     <script src="{{ asset('asset/js/bootstrap.bundle.js') }}"></script>
