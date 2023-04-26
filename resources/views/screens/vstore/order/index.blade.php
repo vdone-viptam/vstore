e34fed5aad0dc5c9c01340b3e8370983fbf853bc
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-26 13:26:09 +0700

    up

--- resources/views/screens/vstore/order/index.blade.php
+++ resources/views/screens/vstore/order/index.blade.php
@@ -206,7 +206,7 @@
 
                 </div>
                 <div class="d-flex align-items-end justify-content-end mt-4">
-                    {{$orders->withQueryString()->onEachSide(10)->links()}}
+                    {{$orders->withQueryString()->links('layouts.custom.paginator')}}
                     <div class="float-right mt-4 ml-3">
                         <form>
                             <div class="form-group">
