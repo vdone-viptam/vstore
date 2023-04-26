a9db08fdf196341dd0ecc36517df4b86a4bf1cb0
phongngo2002 <phongcuoi2213@gmail.com>
2023-04-26 14:14:56 +0700

    up

--- resources/views/layouts/custom/paginator.blade.php
+++ resources/views/layouts/custom/paginator.blade.php
@@ -9,7 +9,7 @@
             <li class="hidden-xs"><a href="{{ $paginator->url(1) }}">1</a></li>
         @endif
         @if($paginator->currentPage() > 4)
-            <li style="border: none !important;"><span>...</span></li>
+            <li style="border: none !important;cursor: none"><span>...</span></li>
         @endif
         @foreach(range(1, $paginator->lastPage()) as $i)
             @if($i >= $paginator->currentPage() - 1 && $i <= $paginator->currentPage() + 1)
@@ -21,10 +21,11 @@
             @endif
         @endforeach
         @if($paginator->currentPage() < $paginator->lastPage() - 3)
-            <li style="border: none !important;"><span>...</span></li>
+            <li style="border: none !important;cursor: none"><span>...</span></li>
         @endif
         @if($paginator->currentPage() < $paginator->lastPage() - 2)
-            <li class="hidden-xs"><a href="{{ $paginator->url($paginator->lastPage()) }}">{{ $paginator->lastPage() }}</a></li>
+            <li class="hidden-xs"><a
+                    href="{{ $paginator->url($paginator->lastPage()) }}">{{ $paginator->lastPage() }}</a></li>
         @endif
         @if ($paginator->hasMorePages())
             <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">»</a></li>
