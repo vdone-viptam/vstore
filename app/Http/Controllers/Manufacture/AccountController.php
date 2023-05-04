2e6facc128990c8e8aab27f5fe3ae5aa4559080b
phongngo2002 <phongcuoi2213@gmail.com>
2023-05-04 11:16:32 +0700

    up

--- app/Http/Controllers/Manufacture/AccountController.php
+++ app/Http/Controllers/Manufacture/AccountController.php
@@ -306,6 +306,7 @@ class AccountController extends Controller
             if (DB::table('request_change_taxcode')->where('tax_code', $request->tax_code)->where('status', 0)->first()) {
                 return redirect()->back()->withErrors(['tax_code' => 'Mã số thuế đã được đăng ký'])->withInput($request->all());
             }
+            $code = rand(10000000, 99999999);
             while (true) {
                 $id = RequestChangeTaxCode::where('code', $code)->count();
                 if ($id == 0) {
