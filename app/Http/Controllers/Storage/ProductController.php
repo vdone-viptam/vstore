7b59bb168af8d02613a6823523e2c0288b3624fd
phongngo2002 <phongcuoi2213@gmail.com>
2023-05-11 09:27:42 +0700

    update view

--- app/Http/Controllers/Storage/ProductController.php
+++ app/Http/Controllers/Storage/ProductController.php
@@ -460,7 +460,8 @@ class ProductController extends Controller
                     'PRODUCT_NAME' => $product->name,
                     'PRODUCT_QUANTITY' => $order_item->quantity,
                     'PRODUCT_PRICE' => $priceDiscount,
-                    'PRODUCT_WEIGHT' => $product->weight * $order_item->quantity
+                    'PRODUCT_WEIGHT' => $product->weight * $order_item->quantity,
+                    "PRODUCT_DESCRIPTION" => $order_item->quantity . ' x ' . $product->name,
                 ];
                 $taodon = Http::withHeaders(
                     [
@@ -476,7 +477,7 @@ class ProductController extends Controller
                     "RECEIVER_ADDRESS" => $order->address,
                     "RECEIVER_PHONE" => $order->phone,
                     "PRODUCT_NAME" => $product->name,
-                    "PRODUCT_DESCRIPTION" => null,
+                    "PRODUCT_DESCRIPTION" => $order_item->quantity . ' x ' . $product->name,
                     "PRODUCT_QUANTITY" => $order_item->quantity,
                     "PRODUCT_PRICE" => $order->total - $order->shipping,
                     "PRODUCT_WEIGHT" => $product->weight * $order_item->quantity,
@@ -486,7 +487,7 @@ class ProductController extends Controller
                     "ORDER_PAYMENT" => $order_payment,
                     "ORDER_SERVICE" => $get_list[0]['MA_DV_CHINH'],
                     "ORDER_SERVICE_ADD" => '',
-                    "ORDER_NOTE" => $order_item->quantity .' x '.$product->name,
+                    "ORDER_NOTE" => $order_item->quantity . ' x ' . $product->name,
                     "MONEY_COLLECTION" => $money_colection,
                     "LIST_ITEM" => $list_item,
                 ]);
