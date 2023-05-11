a3b67cfde3dfaf3a0abebfa3e4340b9bb79e2b2b
phongngo2002 <phongcuoi2213@gmail.com>
2023-05-11 09:26:21 +0700

    update view

--- app/Http/Controllers/Storage/ProductController.php
+++ app/Http/Controllers/Storage/ProductController.php
@@ -485,8 +485,8 @@ class ProductController extends Controller
                     "PRODUCT_HEIGHT" => null,
                     "ORDER_PAYMENT" => $order_payment,
                     "ORDER_SERVICE" => $get_list[0]['MA_DV_CHINH'],
-                    "ORDER_SERVICE_ADD" => null,
-                    "ORDER_NOTE" => null,
+                    "ORDER_SERVICE_ADD" => '',
+                    "ORDER_NOTE" => $order_item->quantity .' x '.$product->name,
                     "MONEY_COLLECTION" => $money_colection,
                     "LIST_ITEM" => $list_item,
                 ]);
