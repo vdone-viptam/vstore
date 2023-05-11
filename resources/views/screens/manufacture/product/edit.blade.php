69ac385f8c410a767cae116b27ec8e84c6ae84ce
Khoa Tran <ttkhoa1999@gmail.com>
2023-05-11 14:11:38 +0700

    IP

--- resources/views/screens/manufacture/product/edit.blade.php
+++ resources/views/screens/manufacture/product/edit.blade.php
@@ -134,15 +134,35 @@
                             <div class="mb-3 col-12 col-xl-6">
                                 <label for="formFileMultiple" class="form-label">Hình ảnh sản phẩm<span
                                         class="text-danger">*</span></label>
-                                <input class="form-control form-control-lg" accept=".jpeg.gif,.png,.jpg" type="file"
-                                       name="images[]"
+                                    <!-- <input class="form-control form-control-lg" accept=".jpeg.gif,.png,.jpg" type="file" name="images[]"
                                        id="images"
                                        multiple>
                                 <p class="text-danger mt-2 ml-1" id="error"></p>
-
                                 @error('images')
                                 <p class="text-danger mt-2 ml-1">{{$message}}</p>
                                 @enderror
+                                @if ($errors->has('images.*'))
+                                    <p class="text-danger mt-2 ml-1">{{ $errors->first('images.*')}}</p>
+                                @endif -->
+                                <div class="images-product">
+                                   <div class="img-item hidden">
+
+                                   </div>
+                                    <input type="hidden" class="input-image">
+                                    <div class="img-event">
+                                            <div class=" add-img-SP">
+                                                <svg width="14" height="14" class="cursor-pointer" viewBox="0 0 14 14" fill="none"
+                                                        xmlns="http://www.w3.org/2000/svg">
+                                                    <path
+                                                        d="M7.46838 1.37598H6.53088C6.44755 1.37598 6.40588 1.41764 6.40588 1.50098V6.40723H1.75C1.66667 6.40723 1.625 6.44889 1.625 6.53223V7.46973C1.625 7.55306 1.66667 7.59473 1.75 7.59473H6.40588V12.501C6.40588 12.5843 6.44755 12.626 6.53088 12.626H7.46838C7.55172 12.626 7.59338 12.5843 7.59338 12.501V7.59473H12.25C12.3333 7.59473 12.375 7.55306 12.375 7.46973V6.53223C12.375 6.44889 12.3333 6.40723 12.25 6.40723H7.59338V1.50098C7.59338 1.41764 7.55172 1.37598 7.46838 1.37598Z"
+                                                        fill="black" fill-opacity="0.85"/>
+                                                </svg>
+                                                <span style="font-size: 14px;">Tải hình ảnh</span>
+                                                <span class="countImage" style="font-size: 12px;">0/5</span>
+                                            </div>
+                                      
+                                    </div>
+                                </div>
                             </div>
                             <div class="col-12 mb-3 col-xl-6">
                                 <div class="form-group" id="file-input">
@@ -572,6 +592,66 @@
 
             }
         })
+        var arrImage = [];
+        
+        function render(arrImg, imgEvent){
+        var htmlImgData = ""
+         arrImg.map((item, index) =>{
+            htmlImgData += `<div class="item" key="${index}">
+            <img style="width:100%; object-fit:cover; border-radius:4px;" src="${item}" />
+            <div class="over-lay-img"></div>
+                    <div class="d-flex justify-content-center align-items-center deleteImg" style="gap:10px">
+                    <svg width="20" height="20" class="zoom-img" onclick="zoomImg('${item}')" style="cursor-pointer" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.6822 7.5404C13.9894 3.97433 11.4305 2.17969 8.0001 2.17969C4.56796 2.17969 2.01082 3.97433 0.31796 7.54219C0.250059 7.68597 0.214844 7.843 0.214844 8.00201C0.214844 8.16102 0.250059 8.31805 0.31796 8.46183C2.01082 12.0279 4.56975 13.8225 8.0001 13.8225C11.4322 13.8225 13.9894 12.0279 15.6822 8.46005C15.8197 8.17076 15.8197 7.83505 15.6822 7.5404V7.5404ZM8.0001 12.5368C5.11975 12.5368 3.01082 11.0761 1.52332 8.00112C3.01082 4.92612 5.11975 3.4654 8.0001 3.4654C10.8805 3.4654 12.9894 4.92612 14.4769 8.00112C12.9912 11.0761 10.8822 12.5368 8.0001 12.5368ZM7.92867 4.85826C6.19296 4.85826 4.78582 6.2654 4.78582 8.00112C4.78582 9.73683 6.19296 11.144 7.92867 11.144C9.66439 11.144 11.0715 9.73683 11.0715 8.00112C11.0715 6.2654 9.66439 4.85826 7.92867 4.85826ZM7.92867 10.0011C6.82332 10.0011 5.92867 9.10647 5.92867 8.00112C5.92867 6.89576 6.82332 6.00112 7.92867 6.00112C9.03403 6.00112 9.92868 6.89576 9.92868 8.00112C9.92868 9.10647 9.03403 10.0011 7.92867 10.0011Z" fill="white" ></path>
+                        </svg>
+                    <svg class="delete-one-image" width="20" height="20"  style="cursor-pointer" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.28544 2.14118H5.14258C5.22115 2.14118 5.28544 2.0769 5.28544 1.99833V2.14118H10.714V1.99833C10.714 2.0769 10.7783 2.14118 10.8569 2.14118H10.714V3.4269H11.9997V1.99833C11.9997 1.36797 11.4872 0.855469 10.8569 0.855469H5.14258C4.51222 0.855469 3.99972 1.36797 3.99972 1.99833V3.4269H5.28544V2.14118ZM14.2854 3.4269H1.71401C1.39794 3.4269 1.14258 3.68225 1.14258 3.99833V4.56975C1.14258 4.64833 1.20686 4.71261 1.28544 4.71261H2.36401L2.80508 14.0519C2.83365 14.6608 3.33722 15.1412 3.94615 15.1412H12.0533C12.664 15.1412 13.1658 14.6626 13.1944 14.0519L13.6354 4.71261H14.714C14.7926 4.71261 14.8569 4.64833 14.8569 4.56975V3.99833C14.8569 3.68225 14.6015 3.4269 14.2854 3.4269ZM11.9158 13.8555H4.08365L3.65151 4.71261H12.3479L11.9158 13.8555Z" fill="white"/>
+                    </svg>
+                    </div>
+            </div>`
+        })
+        if(arrImg.length > 1){
+            $('.img-item').html(htmlImgData)
+        }else{
+            $('.img-item').append(htmlImgData)
+        }
+       
+       
+    }
 
+    function zoomImg(img){
+    $('.img-zoom-full').html(`<img src="${img}" style="width:100%" />`);
+    $('#modalDetailImg').modal('show');
+    }
+
+$('.img-event').on('click', function(){
+ 
+    let input = document.createElement('input');
+        input.type = 'file';
+        input.multiple = 'multiple'
+        input.onchange = _ => {
+            var files = Array.from(input.files);
+            files.map((file)=>{
+                const reader = new FileReader();
+         
+         return new Promise(resolve => {
+             reader.onload = ev => {
+                 resolve(ev.target.result)
+                arrImage.push(ev.target.result)
+                if (arrImage.length >= 5) {
+                    $(this).addClass("hidden");
+                }
+                $('.img-item').removeClass('hidden')
+                render(arrImage,$(this));
+                
+        }
+                reader.readAsDataURL(file)    
+         })
+            })
+           
+        };
+        input.click();
+        
+})
     </script>
 @endsection
