6aaaac0a65a14950c39b1c15a388155c833f6f8a
phongngo2002 <phongcuoi2213@gmail.com>
2023-05-12 16:12:24 +0700

    update view

--- app/Http/Controllers/Storage/ProductController.php
+++ app/Http/Controllers/Storage/ProductController.php
@@ -539,7 +539,6 @@ class ProductController extends Controller
                 ]);
                 try {
                     if ($order->method_payment != 'COD') {
-                        dd($order->no);
                         $result = $this->backMoney($order->no);
                         dd($result);
                     }
