fc50d25f935e1a00659ae1a42403972af2b25b9b
phongngo2002 <phongcuoi2213@gmail.com>
2023-05-16 16:24:40 +0700

    update view

--- resources/views/layouts/custom/pagi_landing.blade.php
+++ resources/views/layouts/custom/pagi_landing.blade.php
@@ -4,7 +4,7 @@
             @if ($paginator->onFirstPage() && $paginator->currentPage() > 1)
                 <div class="px-2 md:px-4 py-1 cursor-pointer btn-prev hover:bg-[#006FFD] rounded-tl-lg rounded-bl-lg">
                     <a href="{{ $paginator->url(1) }}">
-                        <svg width=" 29" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="20" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M17.4463 12.6308L15.7546 14.6044L17.4463 16.5781" stroke="#3A57E8"
                                   stroke-width="1.5"
                                   stroke-linecap="round" stroke-linejoin="round"/>
@@ -16,7 +16,7 @@
                 </div>
             @elseif($paginator->currentPage() == 1 && $paginator->onFirstPage())
                 <div class="px-2 md:px-4 py-1     rounded-tl-lg rounded-bl-lg" style="cursor: default">
-                    <svg width=" 29" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="20" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M17.4463 12.6308L15.7546 14.6044L17.4463 16.5781" stroke="#ddd"
                               stroke-width="1.5"
                               stroke-linecap="round" stroke-linejoin="round"/>
@@ -28,7 +28,7 @@
             @else
                 <div class="px-2 md:px-4 py-1 cursor-pointer  btn-prev hover:bg-[#006FFD]   rounded-tl-lg rounded-bl-lg">
                     <a href="{{ $paginator->previousPageUrl() }}#sp">
-                        <svg width=" 29" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="20" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M17.4463 12.6308L15.7546 14.6044L17.4463 16.5781" stroke="#3A57E8"
                                   stroke-width="1.5"
                                   stroke-linecap="round" stroke-linejoin="round"/>
@@ -56,12 +56,12 @@
                     @if($i >= $paginator->currentPage() - 2 && $i <= $paginator->currentPage() + 2)
                         @if ($i == $paginator->currentPage())
                             <div
-                                class="px-[13.5px] py-[4px] md:py-[5px] hover:bg-[#006FFD] text-[#3A57E8] page-active hover:text-white cursor-pointer">
+                                class="px-[13.5px] py-[4px] md:py-[5px] hover:bg-[#006FFD] text-[#3A57E8] page-active hover:text-white cursor-pointer" style="height: 100%">
                                 <a class="leading-[28px] text-sm md:text-base">{{ $i }}</a>
                             </div>
                         @else
                             <div
-                                class="hidden-xs px-[13.5px] py-[4px] md:py-[5px] hover:bg-[#006FFD] text-[#3A57E8] hover:text-white cursor-pointer">
+                                class="hidden-xs px-[13.5px] py-[4px] md:py-[5px] hover:bg-[#006FFD] text-[#3A57E8] hover:text-white cursor-pointer" style="height: 100%">
                                 <a href="{{ $paginator->url($i) }}#sp"
                                    class="leading-[28px] text-sm md:text-base">{{ $i }}</a>
                             </div>
@@ -71,7 +71,7 @@
                     @if($i >= $paginator->currentPage() - 2 && $i <= $paginator->currentPage() + 2)
                         @if ($i == $paginator->currentPage())
                             <div
-                                class="px-[13.5px] py-[4px] md:py-[5px] hover:bg-[#006FFD] text-[#3A57E8] page-active hover:text-white cursor-pointer">
+                                class="px-[13.5px] py-[4px] md:py-[5px] hover:bg-[#006FFD] text-[#3A57E8] page-active hover:text-white cursor-pointer" style="height: 100%">
                                 <a class="leading-[28px] text-sm md:text-base">{{ $i }}</a>
                             </div>
                         @else
@@ -101,7 +101,7 @@
             @if ($paginator->hasMorePages())
                     <div class="px-2 md:px-4 py-1 cursor-pointer  btn-prev hover:bg-[#006FFD]  rounded-tr-lg rounded-br-lg">
                         <a href="{{ $paginator->nextPageUrl() }}#sp" rel="next">
-                            <svg width="29" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg width="20" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M11.9209 12.6308L13.6126 14.6044L11.9209 16.5781" stroke="#3A57E8" stroke-width="1.5"
                                       stroke-linecap="round" stroke-linejoin="round"/>
                                 <path d="M14.9658 12.6308L16.6576 14.6044L14.9658 16.5781" stroke="#3A57E8" stroke-width="1.5"
@@ -111,7 +111,7 @@
                     </div>
             @else
                     <div class="px-2 md:px-4 py-1 rounded-tr-lg rounded-br-lg">
-                        <svg width="29" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="20" height="30" viewBox="0 0 29 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M11.9209 12.6308L13.6126 14.6044L11.9209 16.5781" stroke="#ddd" stroke-width="1.5"
                                   stroke-linecap="round" stroke-linejoin="round"/>
                             <path d="M14.9658 12.6308L16.6576 14.6044L14.9658 16.5781" stroke="#ddd" stroke-width="1.5"
